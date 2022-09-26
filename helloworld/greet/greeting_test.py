79467bc26f8b3251524eae23c3040c157db9f278
Leo Liang <leozc@users.noreply.github.com>
2022-09-25 21:42:34 -0700

    bump version

--- helloworld/greet/greeting_test.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2020 Pants project contributors.
-# Licensed under the Apache License, Version 2.0 (see LICENSE).
-
-from helloworld.greet.greeting import Greeter
-
-
-def test_greeter() -> None:
-    greeter = Greeter(languages=["es"], greetings=["good morning"])
-    assert greeter.greet("world") == "Buenos días, world!"
--- py_helloworld/bin.py
+++
@@ -1,4 +0,0 @@
-from py_helloworld import lib
-import numpy as np
-
-print("Fib(5)=%d" % lib.Fib(5))
